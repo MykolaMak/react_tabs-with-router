da48f1a3e59c8d324bfa19968ed8fbf6d3fcf318
MykolaMak <makolamak@gmail.com>
2020-05-25 00:04:24 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8758f7d299f4d9363f87977cceac2196.js"
+  "./precache-manifest.4b626794127df464f216fd717fef3332.js"
 );
 
 self.addEventListener('message', (event) => {
